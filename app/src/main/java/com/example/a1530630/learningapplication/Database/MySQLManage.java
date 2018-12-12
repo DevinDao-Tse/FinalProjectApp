d1d9692894124e1a5241ff22e5ea12dce1951266
Devin <devindaotse@gmail.com>
2018-12-12 13:39:12 -0500

    created database classes and user

---
+++ app/src/main/java/com/example/a1530630/learningapplication/Database/MySQLManage.java
@@ -0,0 +1,4 @@
+package com.example.a1530630.learningapplication.Database;
+
+public class MySQLManage {
+}
---
+++ app/src/main/java/com/example/a1530630/learningapplication/Models/User.java
@@ -0,0 +1,4 @@
+package com.example.a1530630.learningapplication.Models;
+
+public class User {
+}
