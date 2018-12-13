b07ebd22d02aa587b1fc73af8b6e1c9825c44554
Devin <devindaotse@gmail.com>
2018-12-13 10:47:11 -0500

    added buttons etc, links

--- app/src/main/java/com/example/a1530630/learningapplication/Database/MySQLManage.java
+++ app/src/main/java/com/example/a1530630/learningapplication/Database/MySQLManage.java
@@ -1,4 +1,25 @@
 package com.example.a1530630.learningapplication.Database;
 
-public class MySQLManage {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.a1530630.learningapplication.Models.User;
+
+public class MySQLManage extends SQLiteOpenHelper
+{
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "Android_Database2";
+    private static final String TableName = "Login";
+
+    public MySQLManage(Context context){super(context, DATABASE_NAME, null, DATABASE_VERSION);}
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(User.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+    }
 }
--- app/src/main/java/com/example/a1530630/learningapplication/Register.java
+++ app/src/main/java/com/example/a1530630/learningapplication/Register.java
@@ -1,19 +1,27 @@
 package com.example.a1530630.learningapplication;
 
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.a1530630.learningapplication.Database.SQLiteManage;
 import com.example.a1530630.learningapplication.Models.User;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
 public class Register extends AppCompatActivity {
     SharedPreferences sharedPreferences;
     SQLiteManage db;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,26 +35,54 @@ public class Register extends AppCompatActivity {
         sharedPreferences = getSharedPreferences(Login.MyPreferences, Context.MODE_PRIVATE);
         try
         {
-            User newUser = new User();
-            if(newUser != null)
-            {
-                db.addNewUser(newUser);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putInt("UserID",0);
-                editor.putString("Username","");
-                editor.putString("Password","");
-                editor.putString("Email","");
-                editor.putString("FullName","");
-                editor.commit();
+            EditText Username = findViewById(R.id.nameText);
+            EditText Email = findViewById(R.id.emailText);
+            EditText Password = findViewById(R.id.passwordText);
+            EditText ConfirmPassword = findViewById(R.id.confirmPass);
 
-                Toast.makeText(this, "User Account created",Toast.LENGTH_LONG).show();
-                Intent i = new Intent(getApplicationContext(), Main_Menu.class);
-                startActivity(i);
+            String user = Username.getText().toString();
+            String email = Email.getText().toString();
+            String password = Password.getText().toString();
+            String confirmpassword = ConfirmPassword.getText().toString();
+
+            if(user.isEmpty()  || email.isEmpty() || password.isEmpty() || confirmpassword.isEmpty())
+            {
+                Toast.makeText(getApplicationContext(),"Enter user details",Toast.LENGTH_LONG).show();
             }
+            if(!password.equalsIgnoreCase(confirmpassword))
+            {
+                Toast.makeText(getApplicationContext(),"Passwords are not the same",Toast.LENGTH_LONG).show();
+            }
+            else
+                {
+                    User newUser = new User();
+                    newUser.setUsername(user);
+                    newUser.setPassword(password);
+                    newUser.setEmail(email);
+                    newUser.setFullName("blank");
+
+                    if(newUser != null) {
+                        db.addNewUser(newUser);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("UserID", 0);
+                        editor.putString("Username", "");
+                        editor.putString("Password", "");
+                        editor.putString("Email", "");
+                        editor.putString("FullName", "");
+                        editor.commit();
+
+                        Toast.makeText(this, "User Account created", Toast.LENGTH_LONG).show();
+                        Intent i = new Intent(getApplicationContext(), Main_Menu.class);
+                        startActivity(i);
+                    }
+            }
+
         }
         catch(Exception e)
         {
             Toast.makeText(this, e.getMessage(),Toast.LENGTH_LONG).show();
         }
     }
+
+
 }
