517f8f2781d7bce755795b201f9387eba98acee6
Devin <devindaotse@gmail.com>
2018-12-15 22:11:56 -0500

    added side bar on main menu

--- app/src/main/java/com/example/a1530630/learningapplication/Main_Menu.java
+++ app/src/main/java/com/example/a1530630/learningapplication/Main_Menu.java
@@ -3,15 +3,24 @@ package com.example.a1530630.learningapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.a1530630.learningapplication.Database.SQLiteManage;
 
-public class Main_Menu extends AppCompatActivity {
+public class Main_Menu extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
     SQLiteManage db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,32 @@ public class Main_Menu extends AppCompatActivity {
 
         Button logout = findViewById(R.id.logoffBtn);
         Button profile = findViewById(R.id.ProfileBtn);
+
+        dl = (DrawerLayout) findViewById(R.id.drawer_layout);
+        t = new ActionBarDrawerToggle(this, dl,R.string.nav_open, R.string.nav_close);
+        dl.addDrawerListener(t);
+        t.syncState(); getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        dl.addDrawerListener(new DrawerLayout.DrawerListener()
+        {
+            @Override
+            public void onDrawerSlide(@NonNull View view, float v) { }
+            @Override
+            public void onDrawerOpened(@NonNull View view)
+            {
+                SharedPreferences settings = getSharedPreferences(Login.MyPreferences, Context.MODE_PRIVATE);
+                String userName = settings.getString("Username",null);
+                TextView user = findViewById(R.id.nav_header_textView);
+                user.setText(userName);
+            }
+            @Override
+            public void onDrawerClosed(@NonNull View view) {}
+            @Override
+            public void onDrawerStateChanged(int i) {}
+        });
+
+        NavigationView nv = findViewById(R.id.nav_view);
+        nv.setNavigationItemSelectedListener(this);
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,5 +77,38 @@ public class Main_Menu extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(t.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId())
+        {
+            case R.id.nav_menu:
+            {
+
+                return true;
+            }
+            case R.id.nav_profile:
+            {
+
+                return true;
+            }
+            case R.id.nav_settings:
+            {
+
+                return true;
+            }
+            case R.id.nav_exit:
+            {
+
+                return true;
+            }
+        }
+
+        return true;
     }
 }
