613644ec0bcea2c0713127e9c8831420f20c6005
Devin <devindaotse@gmail.com>
2019-01-20 09:35:50 -0500

    created 2nd session page with no recording

--- app/src/main/java/com/example/a1530630/learningapplication/Main_Menu.java
+++ app/src/main/java/com/example/a1530630/learningapplication/Main_Menu.java
@@ -105,8 +105,13 @@ public class Main_Menu extends AppCompatActivity
         l1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                lessonHolder = l1.getContentDescription().toString();
-               WordBox(lessonHolder); //passing the selected lesson
+                //lessonHolder = l1.getContentDescription().toString();
+               //WordBox(lessonHolder); //passing the selected lesson
+                Intent i = new Intent(getApplicationContext(), Session2.class);
+                i.putExtra("Audio", w1.getContentDescription().toString());
+                i.putExtra("Module",moduleHolder);
+                i.putExtra("Lesson",l1.getContentDescription().toString());
+                startActivity(i);
             }
         });
 
