6f7b2c533bb024003d3485404d42beed005c150f
Devin <devindaotse@gmail.com>
2019-01-22 15:19:44 -0500

    added

--- app/src/main/java/com/example/a1530630/learningapplication/Main_Menu.java
+++ app/src/main/java/com/example/a1530630/learningapplication/Main_Menu.java
@@ -108,13 +108,14 @@ public class Main_Menu extends AppCompatActivity
                 //lessonHolder = l1.getContentDescription().toString();
                //WordBox(lessonHolder); //passing the selected lesson
                 Intent i = new Intent(getApplicationContext(), Session2.class);
-                i.putExtra("Audio", w1.getContentDescription().toString());
+                i.putExtra("Audio", "0");
                 i.putExtra("Module",moduleHolder);
                 i.putExtra("Lesson",l1.getContentDescription().toString());
                 startActivity(i);
             }
         });
 
+        /*
         l2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -130,6 +131,7 @@ public class Main_Menu extends AppCompatActivity
                 WordBox(lessonHolder);
             }
         });
+        */
         BOX.show();
     }
 
