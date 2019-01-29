54d79bbfce25d198df3f050a322316e87fc75ed0
Shaniesha <shaniesha.john.94@gmail.com>
2019-01-29 17:52:02 -0500

    checkbox login

--- app/src/main/java/com/example/a1530630/learningapplication/Login.java
+++ app/src/main/java/com/example/a1530630/learningapplication/Login.java
@@ -8,8 +8,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,13 +28,23 @@ public class Login extends AppCompatActivity {
     public static final String MyPreferences ="User";
     SQLiteManage db;
     static final int REQUEST_PERMISSION_KEY = 1;
-
+    public boolean check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         db = new SQLiteManage(this);
+
+        sharedPreferences = getSharedPreferences(MyPreferences, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        if(sharedPreferences.getBoolean("Remember",check) == true)
+        {
+            Intent i = new Intent(this,Main_Menu.class);
+            startActivity(i);
+        }
+        
 
 
        /*String[] PERMISSIONS = {Manifest.permission.RECORD_AUDIO};
@@ -54,7 +68,7 @@ public class Login extends AppCompatActivity {
     {
         EditText LoginUser = findViewById(R.id.LoginUser);
         EditText LoginPassword = findViewById(R.id.LoginPassword);
-
+        CheckBox remembermeBtn = findViewById(R.id.rememberMe);
 
 
         String username = LoginUser.getText().toString();
@@ -70,42 +84,53 @@ public class Login extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"Enter login credentials", Toast.LENGTH_LONG).show();
             }
             else
+            {
+                if(db.Login(username,password))
                 {
-                    if(db.Login(username,password))
-                    {
-                        User user = new User();
-                        user.setUsername(username);
-                        user.setPassword(password);
-                        if(db.getUserInfo(user) !=null) {
-                            SharedPreferences.Editor editor = sharedPreferences.edit();
-                            Integer idRef = user.getUserID();
-                            String usernameRef = user.getUsername();
-                            String passwordRef = user.getPassword();
-                            String emailRef = user.getEmail();
-                            String fullnameRef= user.getFullName();
 
-                            editor.putInt("UserID",idRef);
-                            editor.putString("Username",usernameRef);
-                            editor.putString("Password",passwordRef);
-                            editor.putString("Email",emailRef);
-                            editor.putString("FullName",fullnameRef);
-                            editor.commit();
+                    User user = new User();
+                    user.setUsername(username);
+                    user.setPassword(password);
 
-                            Toast.makeText(this, "Logged In",Toast.LENGTH_LONG).show();
-                            Intent i = new Intent(getApplicationContext(), Main_Menu.class);
-                            startActivity(i);
+
+                    if(db.getUserInfo(user) !=null) {
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                         check = false;
+                        Integer idRef = user.getUserID();
+                        String usernameRef = user.getUsername();
+                        String passwordRef = user.getPassword();
+                        String emailRef = user.getEmail();
+                        String fullnameRef= user.getFullName();
+
+                        if(remembermeBtn.isChecked())
+                        {
+                            check = true;
                         }
-                        else
-                            {
-                                Toast.makeText(getApplicationContext(),"Didn't save", Toast.LENGTH_LONG).show();
-                            }
+                        else check = false;
+
+                        editor.putInt("UserID",idRef);
+                        editor.putString("Username",usernameRef);
+                        editor.putString("Password",passwordRef);
+                        editor.putString("Email",emailRef);
+                        editor.putString("FullName",fullnameRef);
+                        editor.putBoolean("Remember",check);
+                        editor.commit();
+
+                        Toast.makeText(this, "Logged In",Toast.LENGTH_LONG).show();
+                        Intent i = new Intent(getApplicationContext(), Main_Menu.class);
+                        startActivity(i);
                     }
                     else
                     {
-                        Toast.makeText(getApplicationContext(),"Invalid Username or Password",Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(),"Didn't save", Toast.LENGTH_LONG).show();
                     }
-
                 }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Invalid Username or Password",Toast.LENGTH_LONG).show();
+                }
+
+            }
         }
         catch(Exception e)
         {
@@ -125,3 +150,15 @@ public class Login extends AppCompatActivity {
     }
     */
 }
+    /*
+    private void closeKeyboard()
+    {
+        View view = this.getCurrentFocus();
+        if(view != null)
+        {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+        }
+    }
+    */
+
